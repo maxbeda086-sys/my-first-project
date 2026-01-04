61d8921a3f29124564944fc627b80d05a636724b
AYE BEDA MAX DUHAMEL <maxbeda086@gmail.com>
2026-01-04 15:43:54 +0000

    Running Flask

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template ("index.html", current_time= datetime.now())
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
+
+
+
 
